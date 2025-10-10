93ff5b87f8870191c533c0d8d07d2b61aaab938a
yachuanilade-ship-it <yachuanilade@gmail.com>
2025-10-10 12:26:21 +0530

    Add files via upload

---
+++ LiberaryaManagement/Books.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    internal class Book
+    {
+        protected string title;
+        protected string author;
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Book Name:{title}");
+            Console.WriteLine($"Book Author:{author}");
+        }
+
+    }
+    class EBook : Book
+    {
+        double filesize;
+        string format;
+
+        public EBook(string title, string author, double filesize, string format) : base(title, author)
+        {
+            this.filesize = filesize;
+            this.format = format;
+        }
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($"Book Name:{title}");
+            Console.WriteLine($"Book Author:{author}");
+            Console.WriteLine($"File Size:{filesize}");
+            Console.WriteLine($"Format:{format}");
+
+
+        }
+
+
+    }
+}
---
+++ LiberaryaManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book("Game of thrones", "G.R.R Martin");
+            b.DisplayDetails();
+            Book e = new EBook("The lord of rings", "J.R.R Tolkien", 900, "EPUB");
+            e.DisplayDetails();
+        }
+    }
+}
