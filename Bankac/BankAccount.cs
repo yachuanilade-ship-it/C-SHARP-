b650d5c85a4128a8aa1f3f1a82b94b77676e4b7c
yachuanilade-ship-it <yachuanilade@gmail.com>
2025-10-10 12:20:57 +0530

    Add files via upload

---
+++ Bankac/BankAccount.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        protected string name;
+        protected int Accountno;
+        protected double balance = 15000;
+
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(string name, int AccountNo)
+        {
+            this.name = name;
+            this.Accountno = AccountNo;
+
+        }
+
+        public BankAccount(string name, int AccountNo, int balance)
+        {
+            this.name = name;
+            this.Accountno = AccountNo;
+            this.balance = balance;
+
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited {amount}");
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine($"Balance is {balance}");
+        }
+        public void Withdraw(int amount)
+        {
+            if (balance < amount)
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"Currrent Balance is {balance} after deducting {amount}");
+            }
+
+        }
+    }
+    class SavingsAcc : BankAccount
+    {
+        double Interest = 0;
+        public SavingsAcc()
+        {
+
+        }
+        public SavingsAcc(string name, int AccountNo)
+        {
+            this.name = name;
+            this.Accountno = AccountNo;
+
+        }
+
+        public SavingsAcc(string name, int AccountNo, int balance)
+        {
+            this.name = name;
+            this.Accountno = AccountNo;
+            this.balance = balance;
+
+        }
+
+        public void CalcInterest(double rate, int time)
+        {
+            Interest = balance * (rate / 100) * time;
+        }
+        public void DisplayInterest()
+        {
+            Console.WriteLine($"The interest is {Interest}");
+        }
+        public void AddInterest()
+        {
+            if (Interest <= 0)
+            {
+                Console.WriteLine("No Interest Calculated.");
+            }
+            else
+            {
+                balance += Interest;
+                Console.WriteLine($"Added {Interest} to Balance.");
+            }
+        }
+    }
+}
---
+++ Bankac/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingsAcc p1 = new SavingsAcc("Josh", 10023);
+            p1.Deposit(1000);
+            p1.GetBalance();
+            p1.Withdraw(1000);
+            p1.CalcInterest(6, 2);
+            p1.DisplayInterest();
+            p1.AddInterest();
+            p1.GetBalance();
+
+        }
+    }
+}
