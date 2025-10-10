36e5d9a283f1b7594c4b81a0782427975ad8be6c
yachuanilade-ship-it <yachuanilade@gmail.com>
2025-10-10 12:53:36 +0530

    Add files via upload

---
+++ sq.of a number/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sq.of_a_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
